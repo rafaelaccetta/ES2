11b0575c1f0973e0c2dc8705d8a50b8b771e0ccb
GusMedeiros <gustavomedeiros@id.uff.br>
2025-09-27 10:06:24 -0300

    Map cards json to PlayerCards

--- game/game-logic/Deck.js
+++ game/game-logic/Deck.js
@@ -1,43 +1,46 @@
-import data  from "../public/data/territories_cards.json" with {type: "json"};
+import { PlayerCards } from "./PlayerCards.js"; // Adjust path if necessary
+import data from "../public/data/territories_cards.json" with {type: "json"};
 
 export class Deck {
     #_deck_cur;
     #_deck_next;
-    constructor(){
-        this.#_deck_cur = {};
-        this.#_deck_next = {};
+
+    constructor() {
+        this.#_deck_cur = [];
+        this.#_deck_next = [];
     }
+
     loadCards() {
-
-
-        this.#_deck_cur = data
-        this.shuffle()
-
+        this.#_deck_cur = Object.entries(data).map(([name, geometricShape]) => {
+            return new PlayerCards({ name, geometricShape });
+        });
+        this.shuffle();
     }
 
     shuffle() {
-        // uma vez carregados os cards, realizar um sort
         for (let i = this.#_deck_cur.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [this.#_deck_cur[i], this.#_deck_cur[j]] = [this.#_deck_cur[j], this.#_deck_cur[i]];
         }
     }
-
+    
     draw() {
-        let card = this.#_deck_cur.pop()
-        this.#_deck_next.push(card)
-        if(this.#_deck_cur.length <= 0){
-            // switch current deck and next deck
-            let temp = this.#_deck_cur
-            this.#_deck_cur = this.#_deck_next
-            this.#_deck_next = temp
-            this.shuffle()
+        if (this.#_deck_cur.length === 0) {
+            this.#_deck_cur = this.#_deck_next;
+            this.#_deck_next = [];
+            this.shuffle();
         }
-        return card
-    }
 
-    show(){
-        // mostra o deck_cur 
-        console.log(this.#_deck_cur)
+        const card = this.#_deck_cur.pop();
+        this.#_deck_next.push(card);
+
+        return card;
+    }
+    
+    show() {
+        console.log("--- Deck Status ---");
+        console.log(`Current Deck (${this.#_deck_cur.length} cards):`, this.#_deck_cur);
+        console.log(`Discard Pile (${this.#_deck_next.length} cards):`, this.#_deck_next);
+        console.log("-------------------");
     }
 }
