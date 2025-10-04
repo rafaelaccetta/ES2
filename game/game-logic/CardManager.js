bf7b113e0ffd089732c1905b937d323d074f5fa4
GusMedeiros <gustavomedeiros@id.uff.br>
2025-10-04 15:28:43 -0300

    troca de cartas implementada

---
+++ game/game-logic/CardManager.js
@@ -0,0 +1,78 @@
+import { Deck } from "./Deck.js";
+
+export class CardManager {
+    #_deck;
+    #_exchangeCount;
+    #_exchangeBonuses;
+    
+    constructor() {
+        this.#_deck = new Deck();
+        this.#_exchangeCount = 0;
+
+        this.#_exchangeBonuses = [4, 6, 8, 10, 12, 15];
+    }
+    
+    drawCardForPlayer() {
+        const card = this.#_deck.draw();
+        if (!card) {
+            console.warn("Could not draw a card. The deck and discard pile might be empty.");
+        }
+        return card;
+    }
+    
+    exchangeCards(cards) {
+        if (!this.#_isValidSet(cards)) {
+            console.log("Invalid set of cards for exchange.");
+            return 0; // Return 0 armies for an invalid set
+        }
+
+        const bonus = this.#_getCurrentBonus();
+        this.#_exchangeCount++;
+        
+        this.#_deck.discard(cards)
+        
+        return bonus;
+    }
+    
+    #_isValidSet(cards) {
+        if (!cards || cards.length !== 3) {
+            return false;
+        }
+
+        const shapes = cards.map(card => card.geometricShape);
+
+        const wildcardCount = shapes.filter(s => s === 'Wildcard').length;
+        if (wildcardCount > 0) return true; // Any set with a wildcard is valid
+
+        const uniqueShapes = new Set(shapes);
+
+        if (uniqueShapes.size === 1) {
+            return true;
+        }
+
+        if (uniqueShapes.size === 3) {
+            return true;
+        }
+
+        return false;
+    }
+    
+    #_getCurrentBonus() {
+        if (this.#_exchangeCount < this.#_exchangeBonuses.length) {
+            // Use the predefined list for the first few exchanges
+            return this.#_exchangeBonuses[this.#_exchangeCount];
+        } else {
+            // After the list is exhausted, the bonus increases by 5 for each subsequent exchange
+            const lastBonusInList = this.#_exchangeBonuses[this.#_exchangeBonuses.length - 1];
+            const stepsBeyondList = this.#_exchangeCount - (this.#_exchangeBonuses.length - 1);
+            return lastBonusInList + (stepsBeyondList * 5);
+        }
+    }
+
+    /**
+     * A utility method to show the deck's current status.
+     */
+    showDeckStatus() {
+        this.#_deck.show();
+    }
+}
--- game/game-logic/Deck.js
+++ game/game-logic/Deck.js
@@ -1,46 +1,48 @@
-import { PlayerCards } from "./PlayerCards.js"; // Adjust path if necessary
+import {PlayerCards} from "./PlayerCards.js"; // Adjust path if necessary
 import data from "../public/data/territories_cards.json" with {type: "json"};
 
 export class Deck {
-    #_deck_cur;
-    #_deck_next;
+    #draw_pile;
+    #_discard_pile;
 
     constructor() {
-        this.#_deck_cur = [];
-        this.#_deck_next = [];
+        this.#draw_pile = [];
+        this.#_discard_pile = [];
+        this.loadCards()
     }
 
     loadCards() {
-        this.#_deck_cur = Object.entries(data).map(([name, geometricShape]) => {
+        this.#draw_pile = Object.entries(data).map(([name, geometricShape]) => {
             return new PlayerCards({ name, geometricShape });
         });
         this.shuffle();
     }
 
     shuffle() {
-        for (let i = this.#_deck_cur.length - 1; i > 0; i--) {
+        for (let i = this.#draw_pile.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [this.#_deck_cur[i], this.#_deck_cur[j]] = [this.#_deck_cur[j], this.#_deck_cur[i]];
+            [this.#draw_pile[i], this.#draw_pile[j]] = [this.#draw_pile[j], this.#draw_pile[i]];
         }
     }
     
     draw() {
-        if (this.#_deck_cur.length === 0) {
-            this.#_deck_cur = this.#_deck_next;
-            this.#_deck_next = [];
+        if (this.#draw_pile.length === 0) {
+            this.#draw_pile = this.#_discard_pile;
+            this.#_discard_pile = [];
             this.shuffle();
         }
 
-        const card = this.#_deck_cur.pop();
-        this.#_deck_next.push(card);
-
-        return card;
+        return this.#draw_pile.pop();
+    }
+    
+    discard(cards) {
+        this.#_discard_pile.push(...cards)
     }
     
     show() {
         console.log("--- Deck Status ---");
-        console.log(`Current Deck (${this.#_deck_cur.length} cards):`, this.#_deck_cur);
-        console.log(`Discard Pile (${this.#_deck_next.length} cards):`, this.#_deck_next);
+        console.log(`Current Deck (${this.#draw_pile.length} cards):`, this.#draw_pile);
+        console.log(`Discard Pile (${this.#_discard_pile.length} cards):`, this.#_discard_pile);
         console.log("-------------------");
     }
 }
